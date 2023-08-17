20875ec31fa21415e71c6a1e0dbfe16cd02f776b
js9005 <devjs9005@gmail.com>
2023-08-18 08:38:27 +0900

    Feat. vuex store 설정

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,10 @@ import Vue from "vue";
 import Vuex from "vuex";
 Vue.use(Vuex);
 
+import auth from "@/store/auth";
+
 export default new Vuex.Store({
-  state: {},
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  modules: {
+    auth,
+  },
 });
