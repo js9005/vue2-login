a3702e923a994e9b74eb0a51bcf15120efcfed0c
js9005 <devjs9005@gmail.com>
2023-08-18 08:40:55 +0900

    Feat. 로그인 관련 화면

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,37 @@
 <template>
-  <router-view></router-view>
+  <div>
+    <header v-if="authenticated">
+      <div>로그인한 관리자 계정 정보 : {{ authenticated }}</div>
+      <button @click="logout">로그아웃</button>
+    </header>
+
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
+import { mapActions, mapGetters } from "vuex";
 export default {
   name: "App",
+  computed: {
+    ...mapGetters({
+      authenticated: "auth/authUser",
+    }),
+  },
+  methods: {
+    ...mapActions({
+      logoutAction: "auth/logout",
+    }),
+
+    logout() {
+      this.logoutAction();
+    },
+  },
 };
 </script>
+
+<style scoped>
+header {
+  background-color: lightgray;
+}
+</style>
