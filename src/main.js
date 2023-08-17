1efc183a4cedf76f4bfd42545b709d50acc5a799
js9005 <devjs9005@gmail.com>
2023-08-18 08:40:22 +0900

    Feat. 새로고침 처리

--- src/main.js
+++ src/main.js
@@ -5,8 +5,13 @@ import store from "@/store/index";
 
 Vue.config.productionTip = false;
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount("#app");
+store
+  .dispatch("auth/attempt", localStorage.getItem("vue2_login_token"))
+  .then(() => {
+    // attempt로 토큰과 관리자 계정 정보를 저장 후 라우터를 실행해야 로그인의 beforeEnter가 작동함
+    new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount("#app");
+  });
